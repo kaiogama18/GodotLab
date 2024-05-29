aa3786feed0d0c6435dff3eb15aa6837a9b142af
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2024-05-29 11:29:13 -0400

    [update] - more 3 type of enemies

--- Tiny Leps/enemies/enemy.gd
+++ Tiny Leps/enemies/enemy.gd
@@ -2,7 +2,23 @@ class_name Enemy
 extends Node2D
 
 @export var health: int =  10
+@export var death_prefab: PackedScene
 
 func damage(amount: int):
 	health -= amount
-	print("Dano de ", amount,  " vida total -> ", health)
+	
+	# Modulate change
+	modulate = Color.RED
+	var tween = create_tween().tween_property(self, "modulate", Color.WHITE, 0.3)
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_QUINT)
+	
+	if health <= 0:
+		die()
+
+func die():
+	if death_prefab:
+		var death_object = death_prefab.instantiate()
+		death_object.position = position
+		get_parent().add_child(death_object)
+	queue_free()
