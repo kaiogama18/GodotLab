b2d9edd45c5bb4e9dea1dd8893fb940a62666084
Kaio Barbosa Gama <kaioeduardoescar@gmail.com>
2024-06-01 01:54:29 -0400

    [add] - drop enemies

--- Tiny Leps/enemies/enemy.gd
+++ Tiny Leps/enemies/enemy.gd
@@ -1,11 +1,17 @@
 class_name Enemy
 extends Node2D
 
+@export_category("Life")
 @export var health: int =  10
 @export var death_prefab: PackedScene
 var damage_digit_prefab: PackedScene
 
 @onready var damage_digit_market = $DamageDigitMarker
+
+@export_category("Drops")
+@export var drop_chance: float = 0.1
+@export var drop_items: Array[PackedScene]
+@export var drop_chances: Array[float]
 
 func _ready():
 	damage_digit_prefab = preload("res://misc/damage_digit.tscn")
@@ -35,4 +41,34 @@ func die():
 		var death_object = death_prefab.instantiate()
 		death_object.position = position
 		get_parent().add_child(death_object)
+	if randf() <= drop_chance:
+		drop_item()	
 	queue_free()
+
+func drop_item():
+	var drop = get_radom_drop_item().instantiate()
+	drop.position = position
+	get_parent().add_child(drop)
+
+func get_radom_drop_item() -> PackedScene:
+	if drop_items.size() == 1:
+		return drop_items[0]
+		
+	# Calculate the max chance
+	var max_change: float = 0.0
+	for drop_chance in drop_chance:
+		max_change += drop_chance
+	
+	#Play a dice
+	var random_value = randf() * max_change
+	
+	#Play the Spin Wheel
+	var needle: float = 0.0
+	for i in drop_items.size():
+		var drop_item = drop_items[i]
+		var drop_chance = drop_chances[i] if i < drop_chances.size() else 1
+		if random_value <= drop_chance + needle:
+			return drop_item
+		needle += drop_chance
+		
+	return drop_items[0]
--- Tiny Leps/systems/mob_spawner.gd
+++ Tiny Leps/systems/mob_spawner.gd
@@ -1,10 +1,12 @@
+class_name MobSpawner
 extends Node2D
 
 @export var creatures: Array[PackedScene]
-@export var mobs_per_minute: float = 60.0
+
 @onready var path_follow_2d: PathFollow2D = %PathFollow2D
 
 var cooldown: float = 0.0
+var mobs_per_minute: float = 60.0
 
 func _process(delta):
 	#Timer (cooldown)
@@ -15,12 +17,20 @@ func _process(delta):
 	var interval = 60.0 / mobs_per_minute
 	cooldown = interval
 	
+	# Ckech is the ppoint is valid
+	var point = get_point()
+	var world_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = point
+	parameters.collision_mask = 0b1001
+	var result: Array = world_state.intersect_point(parameters, 1)
+	if not result.is_empty(): return
+	
 	# Instantiate a new enemy
 	var index = randi_range(0, creatures.size() - 1)
 	var creature_scene = creatures[index]
-	var point = get_point()
 	var creature = creature_scene.instantiate()
-	creature.global_position = get_point()
+	creature.global_position = point
 	get_parent().add_child(creature)
 
 func get_point() -> Vector2:
